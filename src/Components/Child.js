25cafb8e35176e702fa4478d94aee05a1a6a0a47
venati sasi <sasirekha.ca22@gmail.com>
2023-01-09 10:11:27 +0530

    child.js changed

--- src/Components/Child.js
+++ src/Components/Child.js
@@ -4,7 +4,7 @@ class Child extends React.Component{
     render(){
         return(
             <div>
-                <h2>Hello Child</h2>
+                <h2>Props used</h2>
                 <h2>{this.props.xyz}</h2>
             </div>
         )
