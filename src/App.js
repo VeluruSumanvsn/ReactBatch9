abbd5963c18da958b85d71d26901ab4d61ae453d
Sarika Thalluru <sarikathalluru91@gmail.com>
2023-01-12 17:33:32 +0530

    New components are added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import ChangeName from "./Components/ChangeName";
 import Counter from "./Components/Counter";
 import Header from "./Components/Header";
@@ -10,12 +11,14 @@ import Child from "./Components/Child";
 import Usestate from "./Components/Usestate";
 import Guest1 from "./Components/Guest1";
 import Surendra from "./Components/Surendra";
+import Dataloop from "./Components/Dataloop";
+import Userouter from "./Components/Userouter";
 
 function App() {
   return (
 
     <React.Fragment>
-      {/* <Header /> 
+       <Header /> 
        <Child />
        <Navbar /> 
        <Counter />
@@ -24,8 +27,14 @@ function App() {
        <Funcbased />
        <Table />
        <Usestate />
-       <Guest1 /> */}
+       <Guest1 /> 
        <Surendra />
+       <Dataloop />
+       <BrowserRouter>
+       <Routes>
+        <Route path="/Userouter" element={<Userouter />} />
+       </Routes>
+       </BrowserRouter>
     </React.Fragment>
     
     
